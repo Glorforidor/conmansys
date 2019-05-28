ac9940dafa59c29a7f795026f84b281a7a2a283a
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-05-28 16:40:52 +0200

    add stringer to structs
    Signed-off-by: Peter Bom Jakobsen <pejak1990@gmail.com>

--- insservice/storage/service.go
+++ insservice/storage/service.go
@@ -11,8 +11,22 @@ type Item struct {
 	Version string `json:"version,omitempty"`
 }
 
+func (i *Item) String() string {
+	return fmt.Sprintf(
+		"ID: %v, Value: %q, Type: %q, Version: %q",
+		i.ID, i.Value, i.Type, i.Version,
+	)
+}
+
 type Module struct {
 	ID      int64  `json:"id"`
 	Value   string `json:"value,omitempty"`
 	Version string `json:"version,omitempty"`
+}
+
+func (m *Module) String() string {
+	return fmt.Sprintf(
+		"ID: %v, Value: %q, Version: %q",
+		m.ID, m.Value, m.Version,
+	)
 }
