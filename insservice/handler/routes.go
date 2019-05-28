c44f3ef8647959326e79026de9913a235ccc02a0
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-05-28 16:50:54 +0200

    change and add endpoint to new functionallity
    Make it default to only give items of asked modules and return list of
    dependent modules back.
    Add another endpoint to to traverse every dependencies of the given
    modules and return all items including the given modules.
    
    Signed-off-by: Peter Bom Jakobsen <pejak1990@gmail.com>

--- insservice/handler/routes.go
+++ insservice/handler/routes.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/Glorforidor/conmansys/insservice/storage"
 	"github.com/gorilla/mux"
@@ -23,13 +25,24 @@ func New(service storage.Service) http.Handler {
 
 	h := handler{service}
 
-	r.HandleFunc("/insfile", responseJSON(h.insfile)).Methods(http.MethodPost) // default json
-	r.HandleFunc("/insfile/text", responseText(h.insfile)).Methods(http.MethodPost)
+	r.HandleFunc("/insfile", responseJSONWithModules(h.insfileWithModules)).Methods(http.MethodPost)
+	r.HandleFunc("/insfile/text", responseTextWithModules(h.insfileWithModules)).Methods(http.MethodPost)
+	r.HandleFunc("/insfile/traverse", responseJSON(h.insfile)).Methods(http.MethodPost)
+	r.HandleFunc("/insfile/traverse/text", responseText(h.insfile)).Methods(http.MethodPost)
 
 	return r
 }
 
-// responseText packs data into plain/text format.
+// response is the response body for the client. Items and Modules should be set
+// to an empty slice instead of nil slice. An empty error should be stored as
+// nil
+type response struct {
+	Items   []*storage.Item   `json:"items"`
+	Modules []*storage.Module `json:"modules"`
+	Error   *string           `json:"error"`
+}
+
+// responseText packs data into text/plain format.
 func responseText(h func(r *http.Request) (interface{}, int, error)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		data, status, err := h(r)
@@ -41,14 +54,15 @@ func responseText(h func(r *http.Request) (interface{}, int, error)) http.Handle
 		switch v := data.(type) {
 		case []*storage.Item:
 			for _, item := range v {
-				// https://www.ietf.org/rfc/rfc2046.txt says that the newline of plain/text is CRLF
+				// https://www.ietf.org/rfc/rfc2046.txt says that the newline of
+				// text is CRLF
 				fmt.Fprintf(&b, "%v\r\n", item.Value)
 			}
 		default:
 			fmt.Fprintf(&b, "%v\r\n", data)
 		}
 
-		w.Header().Set("Content-Type", "plain/text")
+		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(status)
 		w.Write(b.Bytes())
 	}
@@ -57,33 +71,40 @@ func responseText(h func(r *http.Request) (interface{}, int, error)) http.Handle
 // responseJSON packs data in application/json format.
 func responseJSON(h func(r *http.Request) (interface{}, int, error)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var resp response
 		data, status, err := h(r)
 		if err != nil {
-			data = err.Error()
+			v := err.Error()
+			resp.Error = &v
 		}
 
-		type response struct {
-			Data interface{} `json:"data"`
+		// lets make sure we have an empty collection otherwise json will make
+		// it null
+		resp.Items = []*storage.Item{}
+		resp.Modules = []*storage.Module{}
+
+		switch v := data.(type) {
+		case []*storage.Item:
+			resp.Items = v
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(status)
-		err = json.NewEncoder(w).Encode(response{Data: data})
+		err = json.NewEncoder(w).Encode(resp)
 		if err != nil {
 			log.Printf("could not decode to output: %v", err)
 		}
 	}
 }
 
-func (h handler) insfile(r *http.Request) (interface{}, int, error) {
+func readModules(r io.Reader) ([]storage.Module, int, error) {
 	var modules []storage.Module
-	t, err := ioutil.ReadAll(r.Body)
+	t, err := ioutil.ReadAll(r)
 	if err != nil {
 		return nil, http.StatusBadRequest, fmt.Errorf("could not read request body: %v", err)
 	}
 
 	err = json.Unmarshal(t, &modules)
-
 	if err != nil {
 		return nil, http.StatusBadRequest, fmt.Errorf("could not decode JSON request body: %v", err)
 	}
@@ -94,6 +115,15 @@ func (h handler) insfile(r *http.Request) (interface{}, int, error) {
 		}
 	}
 
+	return modules, 0, nil
+}
+
+func (h handler) insfile(r *http.Request) (interface{}, int, error) {
+	modules, status, err := readModules(r.Body)
+	if err != nil {
+		return nil, status, err
+	}
+
 	items, err := h.storage.GetItems(modules...)
 	if err != nil {
 		log.Println(fmt.Errorf("could not retrieve data from database: %v", err))
@@ -101,4 +131,87 @@ func (h handler) insfile(r *http.Request) (interface{}, int, error) {
 	}
 
 	return items, http.StatusOK, nil
+}
+
+func responseTextWithModules(h func(r *http.Request) ([]interface{}, int, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, status, err := h(r)
+
+		var b bytes.Buffer
+
+		if err != nil {
+			fmt.Fprintf(&b, "%v\r\n", err.Error())
+		} else {
+			sep := strings.Repeat("-", 20) + "\r\n"
+			for _, d := range data {
+				switch v := d.(type) {
+				case []*storage.Item:
+					fmt.Fprint(&b, "items\r\n")
+					fmt.Fprint(&b, sep)
+					for _, item := range v {
+						// https://www.ietf.org/rfc/rfc2046.txt says that the newline of
+						// text is CRLF
+						fmt.Fprintf(&b, "%v\r\n", item.Value)
+					}
+					fmt.Fprint(&b, sep)
+				case []*storage.Module:
+					fmt.Fprint(&b, "modules\r\n")
+					fmt.Fprint(&b, sep)
+					for _, mod := range v {
+						// https://www.ietf.org/rfc/rfc2046.txt says that the newline of
+						// text is CRLF
+						fmt.Fprintf(&b, "%v\r\n", mod.ID)
+					}
+					fmt.Fprint(&b, sep)
+				}
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(status)
+		w.Write(b.Bytes())
+	}
+}
+
+func responseJSONWithModules(h func(r *http.Request) ([]interface{}, int, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var resp response
+		data, status, err := h(r)
+		if err != nil {
+			v := err.Error()
+			resp.Error = &v
+		}
+
+		resp.Items = []*storage.Item{}
+		resp.Modules = []*storage.Module{}
+
+		for _, d := range data {
+			switch v := d.(type) {
+			case []*storage.Item:
+				resp.Items = v
+			case []*storage.Module:
+				resp.Modules = v
+			}
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
+func (h handler) insfileWithModules(r *http.Request) ([]interface{}, int, error) {
+	modules, status, err := readModules(r.Body)
+	if err != nil {
+		return nil, status, err
+	}
+
+	items, mods, err := h.storage.GetItemsAndModules(modules...)
+
+	if err != nil {
+		log.Println(fmt.Errorf("could not retrieve data from database: %v", err))
+		return nil, http.StatusInternalServerError, fmt.Errorf("Ups something went wrong")
+	}
+
+	return []interface{}{items, mods}, http.StatusOK, nil
 }
--- insservice/handler/routes_test.go
+++ insservice/handler/routes_test.go
@@ -48,10 +48,16 @@ var (
 		{Value: "Payment"},
 	}
 
-	service = &serviceMock{items: items}
+	modules = []*storage.Module{
+		{ID: 1},
+		{ID: 2},
+		{ID: 3},
+	}
+
+	service = &serviceMock{items: items, modules: modules}
 )
 
-func TestResponseJSON(t *testing.T) {
+func TestResponseJSONWithModules(t *testing.T) {
 	r := New(service)
 	srv := httptest.NewServer(r)
 	defer srv.Close()
@@ -106,22 +112,118 @@ func TestResponseJSON(t *testing.T) {
 				t.Fatalf("%v", err)
 			}
 
-			data, ok := m["data"]
+			items, ok := m["items"]
 			if !ok {
 				t.Fatal("response body is missing data field")
 			}
 
-			switch v := data.(type) {
+			switch v := items.(type) {
 			case string:
 			case interface{}:
 			default:
 				t.Fatalf("data was of unexpected type: %v", v)
 			}
+
+			modules, ok := m["modules"]
+			if !ok {
+				t.Fatal("response body is missing data field")
+			}
+
+			switch v := modules.(type) {
+			case string:
+			case interface{}:
+			default:
+				t.Fatalf("data was of unexpected type: %v", v)
+			}
+
 		})
 	}
 }
 
-func TestResponseText(t *testing.T) {
+func TestResponseJSON(t *testing.T) {
+	r := New(service)
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
+	url := fmt.Sprintf("%v/insfile/traverse", srv.URL)
+
+	tt := map[string]struct {
+		data   []byte
+		status int
+	}{
+		"success": {
+			data:   []byte("[{\"id\": 1}]\r\n"),
+			status: http.StatusOK,
+		},
+		"fail": {
+			data:   []byte("[{\"d\": 1}]\r\n"),
+			status: http.StatusBadRequest,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			req, _ := http.NewRequest(
+				http.MethodPost,
+				url,
+				bytes.NewReader(tc.data),
+			)
+			resp, err := srv.Client().Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if resp.StatusCode != tc.status {
+				t.Fatalf("expected status: %v, got: %v", http.StatusOK, resp.StatusCode)
+			}
+
+			ct := resp.Header.Get("Content-Type")
+			if ct != "application/json" {
+				t.Fatalf("expected Content-Type: application/json, got: %v", ct)
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("expected to be able to read body: %v", err)
+			}
+
+			if !json.Valid(body) {
+				t.Fatalf("expected valid JSON encoding, got: %v", string(body))
+			}
+
+			m := make(map[string]interface{})
+			if err := json.Unmarshal(body, &m); err != nil {
+				t.Fatalf("%v", err)
+			}
+
+			items, ok := m["items"]
+			if !ok {
+				t.Fatal("response body is missing data field")
+			}
+
+			switch v := items.(type) {
+			case string:
+			case interface{}:
+			default:
+				t.Fatalf("data was of unexpected type: %v", v)
+			}
+
+			modules, ok := m["modules"]
+			if !ok {
+				t.Fatal("response body is missing data field")
+			}
+
+			switch v := modules.(type) {
+			case string:
+			case interface{}:
+			default:
+				t.Fatalf("data was of unexpected type: %v", v)
+			}
+
+		})
+	}
+}
+
+func TestResponseTextWithModules(t *testing.T) {
 	r := New(service)
 	srv := httptest.NewServer(r)
 	defer srv.Close()
@@ -159,8 +261,73 @@ func TestResponseText(t *testing.T) {
 			}
 
 			ct := resp.Header.Get("Content-Type")
-			if ct != "plain/text" {
-				t.Fatalf("expected Content-Type: application/json, got: %v", ct)
+			if ct != "text/plain" {
+				t.Fatalf("expected Content-Type: text/plain, got: %v", ct)
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("expected to be able to read body: %v", err)
+			}
+
+			s := string(body)
+			if tc.status != http.StatusBadRequest {
+				for _, item := range items {
+					if !strings.Contains(s, item.Value) {
+						t.Fatalf("missing value: %v in text body: %v", item.Value, s)
+					}
+				}
+			}
+
+			// other status than OK should result in a body with an error
+			// message
+			if s == "" {
+				t.Fatalf("expected an error message in body")
+			}
+		})
+	}
+}
+
+func TestResponseText(t *testing.T) {
+	r := New(service)
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
+	url := fmt.Sprintf("%v/insfile/traverse/text", srv.URL)
+
+	tt := map[string]struct {
+		data   []byte
+		status int
+	}{
+		"success": {
+			data:   []byte("[{\"id\": 1}]\r\n"), // correct module format
+			status: http.StatusOK,
+		},
+		"fail": {
+			data:   []byte("[{\"d\": 1}]\r\n"), // incorrect module format
+			status: http.StatusBadRequest,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			req, _ := http.NewRequest(
+				http.MethodPost,
+				url,
+				bytes.NewReader(tc.data),
+			)
+			resp, err := srv.Client().Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != tc.status {
+				t.Fatalf("expected status: %v, got: %v", tc.status, resp.StatusCode)
+			}
+
+			ct := resp.Header.Get("Content-Type")
+			if ct != "text/plain" {
+				t.Fatalf("expected Content-Type: text/plain, got: %v", ct)
 			}
 
 			body, err := ioutil.ReadAll(resp.Body)
@@ -187,7 +354,6 @@ func TestResponseText(t *testing.T) {
 }
 
 func TestInsfile(t *testing.T) {
-
 	tt := map[string]struct {
 		body   io.Reader
 		status int
@@ -218,9 +384,9 @@ func TestInsfile(t *testing.T) {
 
 	for name, tc := range tt {
 		t.Run(name, func(t *testing.T) {
-			req, err := http.NewRequest(http.MethodGet, "/", tc.body)
+			req, err := http.NewRequest(http.MethodPost, "/", tc.body)
 			if err != nil {
-				t.Fatalf("could not create GET request: %v", err)
+				t.Fatalf("could not create POST request: %v", err)
 			}
 
 			h := handler{service}
@@ -229,6 +395,61 @@ func TestInsfile(t *testing.T) {
 				defer func() { service.closed = false }()
 			}
 			_, status, err := h.insfile(req)
+			if err != nil {
+				if !tc.err {
+					t.Errorf("expected no error, got: %v", err)
+				}
+			}
+
+			if status != tc.status {
+				t.Errorf("expected status: %v, got: %v", tc.status, status)
+			}
+		})
+	}
+}
+
+func TestInsfileWithModules(t *testing.T) {
+	tt := map[string]struct {
+		body   io.Reader
+		status int
+		err    bool
+		closed bool
+	}{
+		"Success": {
+			body:   bytes.NewReader([]byte("[{\"id\": 1}]\r\n")),
+			status: http.StatusOK,
+		},
+		"wrong JSON format": {
+			body:   bytes.NewReader([]byte("{\"id\": 1}\r\n")),
+			status: http.StatusBadRequest,
+			err:    true,
+		},
+		"wrong JSON field format": {
+			body:   bytes.NewReader([]byte("[{\"i\": 1}]\r\n")),
+			status: http.StatusBadRequest,
+			err:    true,
+		},
+		"closed storage": {
+			body:   bytes.NewReader([]byte("[{\"id\": 1}]\r\n")),
+			status: http.StatusInternalServerError,
+			err:    true,
+			closed: true,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodPost, "/", tc.body)
+			if err != nil {
+				t.Fatalf("could not create POST request: %v", err)
+			}
+
+			h := handler{service}
+			if tc.closed {
+				service.closed = true
+				defer func() { service.closed = false }()
+			}
+			_, status, err := h.insfileWithModules(req)
 			if err != nil {
 				if !tc.err {
 					t.Errorf("expected no error, got: %v", err)
